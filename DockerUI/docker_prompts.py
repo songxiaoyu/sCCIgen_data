41962879f592509a8811664e95cec2ea8f7522c9
Anna Calinawan <anna@Annas-MacBook-Pro.local>
2023-02-08 13:33:07 -0500

    suppresses r warnings while showing  showing errors

--- DockerUI/docker_prompts.py
+++ DockerUI/docker_prompts.py
@@ -1,21 +1,13 @@
-# ---
-# jupyter:
-#   jupytext:
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.14.4
-#   kernelspec:
-#     display_name: st_simulator
-#     language: python
-#     name: st_simulator
-# ---
+#!/usr/bin/env python
+# coding: utf-8
 
-# +
+# In[11]:
+
+
 import glob
 import itertools
 from math import isclose
+import logging
 import os
 import shutil
 import sys
@@ -24,10 +16,14 @@ import numpy as np
 import pandas as pd
 import rpy2.robjects as robjects
 from rpy2.robjects.packages import importr
+from rpy2.rinterface_lib.callbacks import logger as rpy2_logger
 
+rpy2_logger.setLevel(logging.ERROR)
 base = importr('base')
 
-# -
+
+# In[8]:
+
 
 valid_yes_no = ['y', 'n']
 workdir_files = [f for f in glob.glob('working_directory' + '/**', recursive=True) if os.path.isfile(f)]
@@ -35,7 +31,9 @@ workdir_files = [f for f in glob.glob('working_directory' + '/**', recursive=Tru
 
 # # Helper functions
 
-# +
+# In[9]:
+
+
 def print_workdir_files():
     if len(workdir_files) == 0:
         raise ValueError('There are no valid files in the working directory mounted to this container.')
@@ -56,14 +54,20 @@ def print_r_objects(rdata):
     r_obj_index = int(input("\tWhich object is the expression data: \t").lower())
     return rdata[r_obj_index - 1]
 
+def isfloat(num):
+    try:
+        float(num)
+        return True
+    except ValueError:
+        return False
 
-
-# -
 
 # # question0_1
 # - determines workflow
 
-# +
+# In[ ]:
+
+
 print("\n")
 valid_question0_1 = ['1', '2', '3']
 print("\tAvailable tasks ")
@@ -78,10 +82,13 @@ question0_1 = input("\tSelect your task (1/2/3): \t").lower()
 if question0_1 not in valid_question0_1:
     raise ValueError(f'Please enter 1, 2, or 3 to select a dataset. You entered {question0_1}') 
 print("\n")
-# -
+
 
 # # Workflow1
 # - Download pre-simulated data set
+
+# In[ ]:
+
 
 if question0_1 == '1':
     question1_1_filepaths = {
@@ -105,12 +112,17 @@ if question0_1 == '1':
 
     sys.exit()
 
+
 # # Workflow3
 # - Use premade config file
+
+# In[ ]:
+
 
 # this is first because question2 is much longer
 if question0_1 == '3':
     sys.exit()
+
 
 # # Workflow2
 # - Generate a simulation parameter file.
@@ -118,7 +130,9 @@ if question0_1 == '3':
 # ### Question2_1
 # - Input expression data
 
-# +
+# In[ ]:
+
+
 # Select or read in expression data file
 parameters = {
     'path_to_input_dir': 'working_directory/'
@@ -145,7 +159,7 @@ print('\n')
 if question2_1 not in question2_1_filepaths:
     # this probably should ultimately be a while loop
     raise ValueError(f'Please enter 1, 2, 3, or 4 to select an expression data set. You entered {question2_1}') 
-print('selection: ', question2_1_filepaths[question2_1])
+
 if question2_1_filepaths[question2_1] != 'user_input':
     question2_1_download = input("\tWould you like to download this data table locally (y/n)?  ").lower()
     if question2_1_download in valid_yes_no:
@@ -161,7 +175,10 @@ else:
 
 parameters['expression_data_file'] = question2_1_filepaths[question2_1]
 
-# +
+
+# In[ ]:
+
+
 # Question2_1 continued
 # Identify expression data file type
 
@@ -176,7 +193,10 @@ if expression_data_file_extension not in file_type_dict:
 
 parameters['expression_data_file_type'] = file_type_dict[expression_data_file_extension]
 
-# +
+
+# In[ ]:
+
+
 # Question2_1 continued
 # Read expression data and identify cell types
 if parameters['expression_data_file_type'] == 'Rdata': 
@@ -195,12 +215,14 @@ print('\n')
 print(f'\tUnique cell types detected in expression data: {len(expression_data_cell_types)}')
 
 parameters['expression_data_cell_types'] = expression_data_cell_types
-# -
+
 
 # ### Question2_2
 # - Input spatial data
 
-# +
+# In[ ]:
+
+
 question2_2_filepaths = {
     '1': 'NULL',
     '2': 'MERFISH_spatial_map',
@@ -224,7 +246,9 @@ if question2_2 not in question2_2_filepaths:
     raise ValueError(f'Please enter 1, 2, 3, or 4 to select an expression data set. You entered {question2_2}') 
 
 
-# +
+# In[ ]:
+
+
 if question2_2 != '1':
     parameters['simulate_spatial_data'] = 'FALSE'
     if question2_2_filepaths[question2_2] != 'user_input':
@@ -244,10 +268,12 @@ else:
 
 parameters['spatial_data_file'] = question2_2_filepaths[question2_2]
 
-# -
 
 # #### Question2_4
 # - Simulate spatial map using parameters.
+
+# In[ ]:
+
 
 if question2_2 == '1':
     # Simulate spatial map using parameters.
@@ -275,14 +301,14 @@ if question2_2 == '1':
     parameters['num_regions'] = question2_4_2
 
     print("\n")
-    question2_4_3a = input("\tUse default cell-type proportions in each region, where all regions equal to cell-type proportions of the input expression data? (y/n) \t\n").lower()
+    question2_4_3a = input("\tInput custom cell-type proportions in each region (y/n)? [default: all regions equal to cell-type proportions of the input expression data] \t").lower()
     
-    if question2_4_3a == 'y':
-        print('\n')
+    if question2_4_3a == 'n' or question2_4_3a == '':
         print('\tUsing default cell-type proportions.')
-        parameters['use_default_regions'] = 'TRUE'
+        print('\n')
+        parameters['custom_cell_type_proportions'] = 'FALSE'
     else:
-        parameters['use_default_regions'] = 'FALSE'
+        parameters['custom_cell_type_proportions'] = 'TRUE'
         question2_4_3 = []
 
         expression_data_cell_types_proportions = []
@@ -316,35 +342,187 @@ if question2_2 == '1':
             if not isclose(total_proportions, 1):
                 raise ValueError('Cell type proportions for a given region must sum up to 1.')
 
-    question2_4_4 = input("\tSpecify cell-cell inhibition/attraction (y/n)? \t\n").lower()
+    question2_4_4 = input("\tSpecify cell-cell inhibition/attraction (y/n)? [default = NULL]\t").lower()
+    
+    if question2_4_4 == 'n' or question2_4_4 == '':
+        print('\tUsing default NULL.')
+        print('\n')
+        parameters['custom_cell_location_interactions'] = 'FALSE'
+    else:
+        parameters['custom_cell_location_interactions'] = 'TRUE'
+        question2_4_4 = []
+        #TODO: prompts for custom cell location interactions
+    
+    question2_4_5 = input("\tThis parameter adjusts cells to be evenly distributed on a slide (rxange: 0-1). [default = 0]\t").lower()
+    
+    if question2_4_5 == '':
+        print('\tUsing default 0.')
+        print('\n')
+        question2_4_5 = '0'
 
-    # if question2_4_4 == 'n':
-    #TODO: question2_4_4
-    parameters['custom_cell_location_interactions'] = 'FALSE'
-    # else:
-    #     parameters['custom_cell_location_interactions'] = 'TRUE'
-    #     cell_pairs = 
+    if not question2_4_5.isnumeric():
+        raise ValueError(f'Please enter a value between 0 and 1.')
+
+    parameters['cell_even_distribution'] = question2_4_5
+
+    question2_4_6 = input("\tCells closer than this cutoff will be considered overlapping to each other and all but one will be removed (range: 0-0.1 of the slide length/width) [default = 0.02]:\t")
+    
+    if question2_4_6 == '':
+        print('\tUsing default 0.02')
+        print('\n')
+        question2_4_6 = '0.02'
+
+    if not isfloat(question2_4_6):
+        raise ValueError('Please enter a value between 0 and 0.1')
+        
+    parameters['cell_overlap_cutoff'] = question2_4_6
+
+
+# ### Question2_5
+
+# In[ ]:
+
+
+if question2_2 != '1':
+    print('\n')
+    question2_5 = input("\tDo you want to simulate new cells (y/n)? (The alternative is to simulate new expression data for existing cells).\t").lower()
+
+    if question2_5 not in valid_yes_no:
+        raise ValueError("Please enter y/n.")
+    
+    if question2_5 == 'y':
+        print('\n')
+        question2_5_1 = input("\tNumber of simulated cells [default =10,000]: \t").lower()
+        
+        if question2_5_1 == '':
+            question2_5_1 = '10000'
+            print(f"\tUsing default: {question2_5_1}")
+
+        if not question2_5_1.isnumeric():
+            raise ValueError(f'Please enter an integer for the number of simulated cells. You entered {question2_4_1}')
+        
+        parameters['num_simulated_cells'] = question2_5_1
+
+        print("\n")
+        print("\tAvailable methods for determining window on existing ST data")
+        print("\t-----------------------------------")
+        print("\n")
+        print("\t1) network")
+        print("\t2) rectangle")
+        print("\t3) convex")
+        print('\n')
+
+        question2_5_2 = input("\tMethod for determining window on existing ST data (1/2/3) [default = network]: \t").lower()
+        valid_question2_5_2 = {
+            "1": "network", 
+            "2": "rectangle", 
+            "3": "convex"
+        }
+        
+        if question2_5_2 == '':
+            question2_5_2 = '1'
+            print(f"\tUsing default: {valid_question2_5_2[question2_5_2]}")
+
+        if not question2_5_2.isnumeric():
+            raise ValueError(f'Please enter an integer for the number of simulated cells. You entered {question2_4_1}')
+        
+        parameters['window_method'] = valid_question2_5_2[question2_5_2]
+
 
 # ### Question2_6
 
-# +
-print('**** Provide simulation parameters for expression profiles ****')
-question2_6_1 = input("\tSequencing depth ratio between simulated  and reference data [default =1]? \t").lower()
-  
-expr_depth_ratio = question2_6_1
-if expr_depth_ratio == '':
-        expr_depth_ratio = '1'
-        print('\n\tUsing default: ', expr_depth_ratio)
+# In[5]:
 
-if not expr_depth_ratio.isnumeric():
+
+print("\n")
+print('\t**** Provide simulation parameters for expression profiles ****')
+print("\n")
+question2_6_1 = input("\tSequencing depth ratio between simulated  and reference data [default = 1]? \t").lower()
+  
+if question2_6_1 == '':
+        question2_6_1 = '1'
+        print('\n\tUsing default: ', question2_6_1)
+
+if not question2_6_1.isnumeric():
     raise ValueError(f'Please enter a value between 0 and 1.')
 
-parameters['expr_depth_ratio'] = expr_depth_ratio
-# -
+parameters['expr_depth_ratio'] = question2_6_1
+
+print("\n")
+question2_6_2 = input("\tDo you want to mimic gene-gene correlation of the reference data (y/n)? [default: n, simulates independent genes]\t").lower()
+
+if question2_6_2 == '':
+    question2_6_2 = 'n'
+    print('\n\tUsing default: ', question2_6_2)
+
+if question2_6_2 not in valid_yes_no:
+    raise ValueError('Please enter y/n.')
+
+if question2_6_2 == 'n':
+    parameters['gene_cor'] = 'FALSE'
+else:
+    parameters['gene_cor'] = 'TRUE'
+    print('\n')
+    print_workdir_files()
+    print('\n')
+    question2_6_3 = input("\tUse file for the estimated Gaussian Copula for gene-gene correlation of your expression data [default = NULL]\t").lower()
+    if question2_6_3 == '':
+        question2_6_3 = 'NULL'
+        print('\n\tUsing default: ', question2_6_2)
+    elif not question2_6_3.isnumeric():
+        raise ValueError('Please select a file.')
+    else:
+        question2_6_3 = workdir_files[int(question2_6_3)-1]
+
+    parameters['copula_input'] = ''.join(question2_6_3.split('working_directory/')[1:])
+
+
+# ### Question2_7
+
+# In[6]:
+
+
+# question2_7
+print("\n")
+question2_7 = input("\tNumber of simulated data sets [default = 1]: \t").lower()
+
+if question2_7 == '':
+    question2_7 = '1'
+    print(f"\tUsing default: {question2_7}")
+
+if not question2_7.isnumeric():
+    raise ValueError(f'Please enter an integer for the number of simulated data sets. You entered {question2_7}')
+
+parameters['num_simulated_datasets'] = question2_7
+
+
+# ### Question2_8
+
+# In[ ]:
+
+
+# question2_8
+print("\n")
+question2_8 = input("\tSpecify an umbrella seed for reproducible simulation [default = 123]: \t").lower()
+
+if question2_8 == '':
+    question2_8 = '123'
+    print(f"\tUsing default: {question2_8}")
+
+if not question2_8.isnumeric():
+    raise ValueError(f'Please enter an integer for the umbrella seed. You entered {question2_8}')
+
+parameters['parent_simulation_seed'] = question2_8
+
+simulation_seeds = [str(int(parameters['parent_simulation_seed']) + i) for i in range(0, int(parameters['num_simulated_datasets']))]
+parameters['simulation_seed_for_each_dataset'] = ','.join(simulation_seeds)
+
 
 # # Save parameter file
 
-# +
+# In[7]:
+
+
 print('\n')
 save_param_file = input("\tSave parameters for future use (y/n)? ").lower()
 
@@ -370,6 +548,19 @@ if save_param_file == 'y':
     # Use a parameter file for simulation.
 
 
-# -
+
+# In[1]:
+
+
+try:
+    get_ipython().system('jupyter nbconvert --to script docker_prompts.ipynb')
+except NameError:
+    print('\n')
+    print('\tFinished')
+
+
+# In[ ]:
+
+
 
 
