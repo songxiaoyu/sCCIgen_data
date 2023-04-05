70d2a608e6f5daf9bf2bd1df31954c3f484ca670
Weijia Fu <weijia0528@hotmail.com>
2023-04-05 16:13:16 -0700

    updated run.sh

--- DockerUI/run.sh
+++ DockerUI/run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-BASE_DOCKER_IMAGE="annapamela/st_simulator_test"
+#BASE_DOCKER_IMAGE="annapamela/st_simulator_test"
+BASE_DOCKER_IMAGE="songxiaoyu152/st_simulator_test"
+#BASE_DOCKER_IMAGE="fuweijia/st_simulator_test"
 
+printf "\t-----------Start ST simulator-----------\n"
 # get absolute path to working directory
 read -p "Enter the absolute path to your working directory. Outputs will be saved to this directory, and all inputs must come from this directory: " -e WORKDIR
 
